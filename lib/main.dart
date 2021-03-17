9be4df1b5126df17936388cc9c3a57f935c58655
nadirmosta <jombo4567@gmail.com>
2021-03-17 22:58:07 +0100

    add app

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Myrecipe'),
+
+        ),
+        ),
+    );
+  }
+}
