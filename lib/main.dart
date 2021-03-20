c9934c085fa8aba25e9232fce9af617a5a2bad7a
nadirmosta <jombo4567@gmail.com>
2021-03-20 17:42:16 +0100

    added a listview/button

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-
-          //backgroundColor: Colors.black45,
           appBar: AppBar(
+              //remove appbar shadow
+              elevation: 0,
               toolbarHeight: 50.0,
               backgroundColor: Colors.white,
               title: Row(
@@ -36,45 +36,57 @@ class MyApp extends StatelessWidget {
                 ],
               )),
           body: SafeArea(
-            child: Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(left: 20),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        width: double.infinity,
-                        child: Container(
-                          child: Text(
-                            'Hi User !',
-                            style: TextStyle(color: Colors.grey),
-                            textAlign: TextAlign.left,
+            // all container
+            child: Container(
+              color: Colors.white,
+              child: Column(
+                children: [
+                  // container of text containt
+                  Container(
+                    margin: EdgeInsets.only(left: 20),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          width: double.infinity,
+                          // container of first text
+                          child: Container(
+                            child: Text(
+                              'Hi User !',
+                              style: TextStyle(color: Colors.grey),
+                              textAlign: TextAlign.left,
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        width: double.infinity,
-                        child: Container(
-                            child: RichText(
-                          text: TextSpan(
-                            text: 'make your own food, \n stay at ',
-                            style: TextStyle(color: Colors.black, fontSize: 20),
-                            children: [
-                              TextSpan(
-                                text: 'home',
-                                style: TextStyle(
-                                    color: Colors.orange, fontSize: 20),
-                              )
-                            ],
-                          ),
-                        )),
-                      ),
-                    ],
+                        SizedBox(
+                          width: double.infinity,
+                          // container of second text
+                          child: Container(
+                              child: RichText(
+                            text: TextSpan(
+                              text: 'make your own food, \n stay at ',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                              children: [
+                                TextSpan(
+                                  text: 'home',
+                                  style: TextStyle(
+                                      color: Colors.orange,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              ],
+                            ),
+                          )),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Container(
+                  //container of search bar and settings icon/button
+                  Container(
                     margin:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                        EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
                     child: Row(
                       children: [
                         Expanded(
@@ -87,34 +99,112 @@ class MyApp extends StatelessWidget {
                                   decoration: InputDecoration(
                                     prefixIcon: Icon(Icons.search),
                                     filled: true,
-                                    fillColor: Colors.grey[350],
+                                    fillColor: Colors.grey[100],
                                     labelText: 'find your today recipe',
                                     border: new OutlineInputBorder(
                                       borderRadius: const BorderRadius.all(
-                                        const Radius.circular(12.0),
+                                        const Radius.circular(15.0),
                                       ),
+                                      borderSide: BorderSide.none,
                                     ),
                                   ),
                                 ),
                               ),
                               Container(
-                                margin: EdgeInsets.symmetric(horizontal: 15.0),
+                                margin: EdgeInsets.symmetric(horizontal: 10.0),
                                 decoration: BoxDecoration(
-                                    color: Colors.orange,
-                                    borderRadius: BorderRadius.all(
-                                        const Radius.circular(5.0))),
+                                  color: Colors.orange,
+                                  borderRadius: BorderRadius.all(
+                                    const Radius.circular(15.0),
+                                  ),
+                                ),
                                 child: Container(
-                                    child: Icon(
-                                  Icons.sort,
-                                  size: 40,
-                                )),
+                                  child: Icon(
+                                    Icons.settings,
+                                    size: 50,
+                                    color: Colors.grey[850],
+                                  ),
+                                ),
                               ),
                             ],
                           ),
                         )
                       ],
-                    ))
-              ],
+                    ),
+                  ),
+                  Container(
+                    height: 50.0,
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 7.5),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              shape: new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(15.0),
+                              ),
+                            ),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.food_bank_rounded,
+                                  size: 35,
+                                ),
+                                Text('pizza')
+                              ],
+                            ),
+                          ),
+                        ),
+                        //
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 7.5),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              shape: new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(15.0),
+                              ),
+                            ),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.sports_tennis_outlined,
+                                  size: 35,
+                                ),
+                                Text('tenniiiiiiiis')
+                              ],
+                            ),
+                          ),
+                        ),
+                        //
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 7.5),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              shape: new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(15.0),
+                              ),
+                            ),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.ac_unit,
+                                  size: 35,
+                                ),
+                                Text('Breakfast')
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           )),
     );
